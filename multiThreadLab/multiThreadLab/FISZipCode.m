aa9a573f1b050538790bdb0720b7b6c5bc181c19
Joe Burgess <joe@flatironschool.com>
2014-04-26 16:22:06 -0400

    Adding starter files

---
+++ multiThreadLab/multiThreadLab/FISZipCode.m
@@ -0,0 +1,13 @@
+//
+//  FISZipCode.m
+//  
+//
+//  Created by Joe Burgess on 4/26/14.
+//
+//
+
+#import "FISZipCode.h"
+
+@implementation FISZipCode
+
+@end
---
+++ multiThreadLab/multiThreadLab/FISZipSearchOperation.m
@@ -0,0 +1,13 @@
+//
+//  FISZipSearchOperation.m
+//  multiThreadLab
+//
+//  Created by Joe Burgess on 4/26/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import "FISZipSearchOperation.h"
+
+@implementation FISZipSearchOperation
+
+@end
